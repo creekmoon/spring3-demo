3213291d78b60bb94f3320cc6c2f74c1b432e77c
creekmoon <841710216@qq.com>
2023-07-18 02:18:37 +0800

    +init

--- nacos-discovery-consumer-example/src/main/java/com/alibaba/cloud/examples/configs/PrometheusSupport.java
+++ nacos-discovery-consumer-example/src/main/java/com/alibaba/cloud/examples/configs/PrometheusSupport.java
@@ -1,6 +1,9 @@
 package com.alibaba.cloud.examples.configs;
 
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +17,26 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class PrometheusSupport {
+    /*
+     * 打标签
+     * */
     @Bean
     MeterRegistryCustomizer<MeterRegistry> configurer(
             @Value("${spring.application.name}") String applicationName) {
         return (registry) -> registry.config().commonTags("application", applicationName);
     }
+
+    /* 拓展JVM监控面板依赖  面板ID=4701 */
+    @Bean
+    public MeterBinder processMemoryMetrics() {
+        return new ProcessMemoryMetrics();
+    }
+
+    /* 普罗米修斯JVM监控面板依赖 ID=4701 */
+    @Bean
+    public MeterBinder processThreadMetrics() {
+        return new ProcessThreadMetrics();
+    }
+
 
 }
