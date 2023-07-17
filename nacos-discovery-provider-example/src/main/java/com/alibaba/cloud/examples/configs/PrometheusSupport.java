5a67ab3654df7395496e7104b223c96fbdb5957e
creekmoon <841710216@qq.com>
2023-07-18 01:52:09 +0800

    +init

---
+++ nacos-discovery-provider-example/src/main/java/com/alibaba/cloud/examples/configs/PrometheusSupport.java
@@ -0,0 +1,23 @@
+package com.alibaba.cloud.examples.configs;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 添加自定义标签, 适配监控面板
+ *
+ * @author JY
+ */
+@Configuration
+public class PrometheusSupport {
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+
+}
